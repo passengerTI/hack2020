3bb6d9b3ec23ee56ec0e81b2566483e5a8c770ad
Andrey V. Semenushkin <andrey@semenushkin.ru>
2020-11-28 00:45:05 +0100

    add client details

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^client/(\d{2}\-\d{3}\-\d{3}\-\d{2})$', views.client, name='client'),
 ]
--- api/views.py
+++ api/views.py
@@ -7,21 +7,35 @@ import json
 import cgi
 
 
-
 db = pymysql.connect("localhost","hack2020","simple7Start","hack2020")
 
-cursor = db.cursor()
-cursor.execute("SELECT id, areaType, birthday, age FROM passport LIMIT 10")
-rows = cursor.fetchall()
-
-result = "["
-
-for row in rows:
-  result += "{"
-  result += """ "id":"{0}", "areaType":{1}, "birthday":"{2}", "age":{3}
-  """.format(row[0],row[1],row[2],row[3])
-  result += "},"
-result += "{}]"
-
 def index(request):
+
+    cursor = db.cursor()
+    cursor.execute("SELECT id, areaType, birthday, age FROM passport LIMIT 20")
+    rows = cursor.fetchall()
+
+    result = "["
+
+    for row in rows:
+      result += "{"
+      result += """ "id":"{0}", "areaType":{1}, "birthday":"{2}", "age":{3}""".format(row[0],row[1],row[2],row[3])
+      result += "},"
+    result += "{}]"
+
     return HttpResponse(result)
+
+def client(request,clientId):
+    cursor = db.cursor()
+    cursor.execute("SELECT areaType, birthday, age FROM passport WHERE id like '{0}'".format(clientId))
+    rows = cursor.fetchone()
+
+    result = "[{"
+    result += """ "areaType":"{0}", "birthday":"{1}", "age":"{2}" """.format(rows[0],rows[1],rows[2])
+    result += "}]"
+
+    return HttpResponse(result)
+
+
+#db.close()
+#db2.close()
